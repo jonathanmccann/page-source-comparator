6a9c1d759466e674a8d6f1c5ceef96cece7a4d10
Jonathan McCann <jonathan.mccann@liferay.com>
2014-09-03 08:28:07 -0700

    Write the page source to a file

--- src/com/comparator/Main.java
+++ src/com/comparator/Main.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -12,6 +13,13 @@ public class Main {
 	public static void main (String[] args) throws IOException {
 		for (String input : args) {
 			String urlSource = getUrlSource(input);
+
+			PrintWriter writer = new PrintWriter(
+				"original-page-source.html", "UTF-8");
+
+			writer.println(urlSource);
+
+			writer.close();
 		}
 	}
 
