211260ea4f47b6134b9ec6359e90c756e9ac02d4
Jonathan McCann <jonathan.mccann@liferay.com>
2014-09-03 08:21:47 -0700

    Base logic for getting a page's source

---
+++ src/com/comparator/Main.java
@@ -0,0 +1,39 @@
+package com.comparator;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Main {
+	public static void main (String[] args) throws IOException {
+		for (String input : args) {
+			String urlSource = getUrlSource(input);
+		}
+	}
+
+	private static String getUrlSource(String url) throws IOException {
+		URL sourceUrl = new URL(url);
+
+		URLConnection sourceUrlConnection = sourceUrl.openConnection();
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+			sourceUrlConnection.getInputStream(), "UTF-8"));
+
+		String inputLine = null;
+
+		StringBuilder a = new StringBuilder();
+
+		while ((inputLine = in.readLine()) != null) {
+			a.append(inputLine);
+		}
+
+		in.close();
+
+		return a.toString();
+	}
+
+}
